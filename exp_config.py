e4e51c33b87ed6a70501525b0a54718be48f9b70
Gerald White <geraldkwhite10@gmail.com>
2022-11-03 14:19:24 -0500

    minor changes

--- exp_config.py
+++ exp_config.py
@@ -13,12 +13,12 @@ def get_config():
 
         "criterion_class": nn.CrossEntropyLoss,
         "criterion_usl": nn.MSELoss,
-        "optimizer_type": torch.optim.SGD,
+        "optimizer_type": torch.optim.Adam,
         "loaders_usl": None,
         "epochs_per_layer_usl_init": 1,
         "epochs_per_layer_usl": 1,
         "epochs_classif": 1,
-        "lr_usl": 0.07,
+        "lr_usl": 0.001,
         "lr_classif": 0.01,
         "layers_to_add": 0,
     }
