398efe56181da47da54b7788ab10369b84c03789
Gerald White <geraldkwhite10@gmail.com>
2022-11-03 16:18:39 -0500

    minor changes

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from datetime import datetime
 
 import networks, exp_config, load_data, train
 
+def main():
+    config = exp_config.get_config()
+    config['device'] = torch.device("cpu")
+    config['loaders_usl'] = load_data.get_mnist(config)
+    load_data.make_dir('ExperimentFiles/')
+    config['exp_folder_path'] = 'ExperimentFiles/' + 'lw_ae_exp_' + datetime.now().strftime("%m-%d-%Y_%H-%M-%S") + '/'
 
-config = exp_config.get_config()
-config['device'] = torch.device("cpu")
-config['loaders_usl'] = load_data.get_mnist(config)
-load_data.make_dir('ExperimentFiles/')
-config['exp_folder_path'] = 'ExperimentFiles/' + 'lw_ae_exp_' + datetime.now().strftime("%m-%d-%Y_%H-%M-%S") + '/'
+    model = networks.Linear_AE_LC(config)
 
-model = networks.Linear_AE_LC(config)
+    config['layers_to_add'] = 5
+    config['epochs_per_layer_usl'] = 50
 
-config['layers_to_add'] = 2
-config['epochs_per_layer_usl'] = 10
+    train.ae_train_layerwise(model, config)
+    return
 
-train.ae_train_layerwise(model, config)
 
+if __name__ == "main":
+    main()
