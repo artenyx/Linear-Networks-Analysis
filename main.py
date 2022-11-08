8142bd36cbf760cd7be5fec727dd3d8f96da2dc2
Gerald White <geraldkwhite10@gmail.com>
2022-11-08 15:54:53 -0600

    remove tqdm

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main(args):
 
     config['layers_to_add'] = args.add_layers
     config['layers_per_step'] = args.layers_per_step
-    config['steps'] = args.add_layers / args.layers_per_step
+    config['steps'] = int(args.add_layers / args.layers_per_step)
 
-    config['epochs_per_layer_usl'] = args.epochs_per_layer
+    config['epochs_per_layer_usl'] = args.epochs_per_step
 
     model, data = train.train_ae_layerwise(model, config)
     train.train_classifier(model, config)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_root", type=str, default="ExperimentFiles/")
     parser.add_argument("--add_layers", type=int, required=True, help="Number of layers to add to base model.")
     parser.add_argument("--layers_per_step", type=int, default=1, help="Number of layers to add to per step.")
-    parser.add_argument("--epochs_per_layer", type=int, required=True, help="Number of epochs to run for each layer added.")
+    parser.add_argument("--epochs_per_step", type=int, required=True, help="Number of epochs to run for each layer added.")
     parser.add_argument("--epochs_classif", type=int, required=True, help="Number of epochs to run classifier for after initialization.")
     args = parser.parse_args()
     if args.add_layers % args.layers_per_step != 0:
